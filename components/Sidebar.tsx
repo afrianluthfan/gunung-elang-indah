2bf5b90a8f1031c83375b6be51211bd494a7c9a9
afrianluthfan <55348072+afrianluthfan@users.noreply.github.com>
2024-06-15 19:37:53 +0700

    feat: kelar landing logistik

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,32 +11,72 @@ const Sidebar = () => {
   const router = useRouter();
   const [isActive, setIsActive] = useState("");
   const currentUrl = usePathname();
-  useEffect(() => setIsActive(currentUrl), [isActive]);
+  const [menuItemsList, setMenuItemsList] = useState([
+    { activePage: "", pageName: "", pageRoute: "" },
+  ]);
+  useEffect(() => setIsActive(currentUrl), [currentUrl, isActive]);
+
+  useEffect(() => {
+    const salesMenus = [
+      {
+        activePage: "/profiling",
+        pageName: "Profiling",
+        pageRoute: "profiling",
+      },
+      {
+        activePage: "/proforma-invoice",
+        pageName: "Proforma Invoice",
+        pageRoute: "proforma-invoice",
+      },
+      {
+        activePage: "/stok-barang",
+        pageName: "Stok Barang",
+        pageRoute: "stok-barang",
+      },
+      {
+        activePage: "/sewa-alat",
+        pageName: "Sewa Alat",
+        pageRoute: "sewa-alat",
+      },
+    ];
+
+    const logistikMenus = [
+      {
+        activePage: "/stok-barang",
+        pageName: "Stok Barang",
+        pageRoute: "stok-barang",
+      },
+      {
+        activePage: "/rental-items",
+        pageName: "Rental Items",
+        pageRoute: "rental-items",
+      },
+      {
+        activePage: "/documents",
+        pageName: "Documents",
+        pageRoute: "documents",
+      },
+    ];
+
+    if (isActive === "/profiling") {
+      setMenuItemsList(salesMenus);
+    } else if (isActive === "/stok-barang") {
+      setMenuItemsList(logistikMenus);
+    }
+  }, [isActive]);
 
   return (
     <div className="flex h-screen w-[17.3vw] min-w-[107px] flex-col items-center justify-between bg-[#011869] p-5">
       <SidebarTopItem />
       <SidebarMenuItemsLayout>
-        <SidebarMenuItem
-          active={isActive === "/profiling"}
-          pageName="Profiling"
-          pageRoute="profiling"
-        />
-        <SidebarMenuItem
-          active={isActive === "/proforma-invoice"}
-          pageName="Proforma Invoice"
-          pageRoute="proforma-invoice"
-        />
-        <SidebarMenuItem
-          active={isActive === "/stok-barang"}
-          pageName="Stok Barang"
-          pageRoute="stok-barang"
-        />
-        <SidebarMenuItem
-          active={isActive === "/sewa-alat"}
-          pageName="Sewa Alat"
-          pageRoute="sewa-alat"
-        />
+        {menuItemsList.map((menuItem, index) => (
+          <SidebarMenuItem
+            key={index}
+            active={isActive === menuItem.activePage}
+            pageName={menuItem.pageName}
+            pageRoute={menuItem.pageRoute}
+          />
+        ))}
       </SidebarMenuItemsLayout>
       <Button
         className="w-full font-bold"
---
+++ components/Table/Table.tsx
@@ -0,0 +1,285 @@
+"use client";
+
+import React from "react";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  Button,
+  Chip,
+  User,
+  Pagination,
+  Selection,
+  ChipProps,
+  SortDescriptor,
+  Tooltip,
+} from "@nextui-org/react";
+import { columns, users, statusOptions } from "./data";
+import { DeleteIcon } from "./DeleteIcon";
+import { EyeIcon } from "./EyeIcon";
+import { EditIcon } from "./EditIcon";
+
+const statusColorMap: Record<string, ChipProps["color"]> = {
+  active: "success",
+  paused: "danger",
+  vacation: "warning",
+};
+
+const INITIAL_VISIBLE_COLUMNS = ["name", "jumlah", "harga", "actions"];
+
+type User = (typeof users)[0];
+
+export default function TableComponent() {
+  const [filterValue, setFilterValue] = React.useState("");
+  const [selectedKeys, setSelectedKeys] = React.useState<Selection>(
+    new Set([]),
+  );
+  const [visibleColumns, setVisibleColumns] = React.useState<Selection>(
+    new Set(INITIAL_VISIBLE_COLUMNS),
+  );
+  const [statusFilter, setStatusFilter] = React.useState<Selection>("all");
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [sortDescriptor, setSortDescriptor] = React.useState<SortDescriptor>({
+    column: "age",
+    direction: "ascending",
+  });
+
+  const [page, setPage] = React.useState(1);
+
+  const hasSearchFilter = Boolean(filterValue);
+
+  const headerColumns = React.useMemo(() => {
+    if (visibleColumns === "all") return columns;
+
+    return columns.filter((column) =>
+      Array.from(visibleColumns).includes(column.uid),
+    );
+  }, [visibleColumns]);
+
+  const filteredItems = React.useMemo(() => {
+    let filteredUsers = [...users];
+
+    if (hasSearchFilter) {
+      filteredUsers = filteredUsers.filter((user) =>
+        user.name.toLowerCase().includes(filterValue.toLowerCase()),
+      );
+    }
+    if (
+      statusFilter !== "all" &&
+      Array.from(statusFilter).length !== statusOptions.length
+    ) {
+      filteredUsers = filteredUsers.filter((user) =>
+        Array.from(statusFilter).includes(user.status),
+      );
+    }
+
+    return filteredUsers;
+  }, [hasSearchFilter, statusFilter, filterValue]);
+
+  const pages = Math.ceil(filteredItems.length / rowsPerPage);
+
+  const items = React.useMemo(() => {
+    const start = (page - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+
+    return filteredItems.slice(start, end);
+  }, [page, filteredItems, rowsPerPage]);
+
+  const sortedItems = React.useMemo(() => {
+    return [...items].sort((a: User, b: User) => {
+      const first = a[sortDescriptor.column as keyof User] as number;
+      const second = b[sortDescriptor.column as keyof User] as number;
+      const cmp = first < second ? -1 : first > second ? 1 : 0;
+
+      return sortDescriptor.direction === "descending" ? -cmp : cmp;
+    });
+  }, [sortDescriptor, items]);
+
+  const renderCell = React.useCallback((user: User, columnKey: React.Key) => {
+    const cellValue = user[columnKey as keyof User];
+
+    switch (columnKey) {
+      case "name":
+        return (
+          <User
+            avatarProps={{ radius: "lg", src: user.avatar }}
+            name={cellValue}
+          ></User>
+        );
+      case "role":
+        return (
+          <div className="flex flex-col">
+            <p className="text-bold text-small capitalize">{cellValue}</p>
+            <p className="text-bold text-tiny capitalize text-default-400">
+              {user.team}
+            </p>
+          </div>
+        );
+      case "status":
+        return (
+          <Chip
+            className="capitalize"
+            color={statusColorMap[user.status]}
+            size="sm"
+            variant="flat"
+          >
+            {cellValue}
+          </Chip>
+        );
+      case "actions":
+        return (
+          <div className="relative flex items-center gap-2">
+            <Tooltip content="Details" className="text-black">
+              <span className="cursor-pointer text-lg text-default-400 active:opacity-50">
+                <EyeIcon />
+              </span>
+            </Tooltip>
+            <Tooltip content="Edit user" className="text-black">
+              <span className="cursor-pointer text-lg text-default-400 active:opacity-50">
+                <EditIcon />
+              </span>
+            </Tooltip>
+            <Tooltip color="danger" content="Delete user">
+              <span className="cursor-pointer text-lg text-danger active:opacity-50">
+                <DeleteIcon />
+              </span>
+            </Tooltip>
+          </div>
+        );
+      default:
+        return cellValue;
+    }
+  }, []);
+
+  const onNextPage = React.useCallback(() => {
+    if (page < pages) {
+      setPage(page + 1);
+    }
+  }, [page, pages]);
+
+  const onPreviousPage = React.useCallback(() => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  }, [page]);
+
+  const onRowsPerPageChange = React.useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      setRowsPerPage(Number(e.target.value));
+      setPage(1);
+    },
+    [],
+  );
+
+  const topContent = React.useMemo(() => {
+    return (
+      <div className="flex flex-col gap-4">
+        <div className="flex items-center justify-between">
+          <span className="text-small text-default-400">
+            Total {users.length} users
+          </span>
+          <label className="flex items-center text-small text-default-400">
+            Rows per page:
+            <select
+              className="bg-transparent text-small text-default-400 outline-none"
+              onChange={onRowsPerPageChange}
+            >
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="15">15</option>
+            </select>
+          </label>
+        </div>
+      </div>
+    );
+  }, [onRowsPerPageChange]);
+
+  const bottomContent = React.useMemo(() => {
+    return (
+      <div className="flex items-center justify-between px-2 py-2">
+        <span className="w-[30%] text-small text-default-400">
+          {selectedKeys === "all"
+            ? "All items selected"
+            : `${selectedKeys.size} of ${filteredItems.length} selected`}
+        </span>
+        <Pagination
+          isCompact
+          showControls
+          showShadow
+          color="primary"
+          page={page}
+          total={pages}
+          onChange={setPage}
+        />
+        <div className="hidden w-[30%] justify-end gap-2 sm:flex">
+          <Button
+            isDisabled={pages === 1}
+            size="sm"
+            variant="flat"
+            onPress={onPreviousPage}
+          >
+            Previous
+          </Button>
+          <Button
+            isDisabled={pages === 1}
+            size="sm"
+            variant="flat"
+            onPress={onNextPage}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    );
+  }, [
+    selectedKeys,
+    filteredItems.length,
+    page,
+    pages,
+    onPreviousPage,
+    onNextPage,
+  ]);
+
+  return (
+    <Table
+      aria-label="Example table with custom cells, pagination and sorting"
+      isHeaderSticky
+      bottomContent={bottomContent}
+      bottomContentPlacement="outside"
+      classNames={{
+        wrapper: "max-h-[382px]",
+      }}
+      selectedKeys={selectedKeys}
+      selectionMode="multiple"
+      sortDescriptor={sortDescriptor}
+      topContent={topContent}
+      topContentPlacement="outside"
+      onSelectionChange={setSelectedKeys}
+      onSortChange={setSortDescriptor}
+    >
+      <TableHeader columns={headerColumns}>
+        {(column) => (
+          <TableColumn
+            key={column.uid}
+            align={column.uid === "actions" ? "center" : "start"}
+            allowsSorting={column.sortable}
+          >
+            {column.name}
+          </TableColumn>
+        )}
+      </TableHeader>
+      <TableBody emptyContent={"No users found"} items={sortedItems}>
+        {(item) => (
+          <TableRow key={item.id}>
+            {(columnKey) => (
+              <TableCell>{renderCell(item, columnKey)}</TableCell>
+            )}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+}
