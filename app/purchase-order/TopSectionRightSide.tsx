8992a4dd7a376b54e1be04a1a2524c02dabf3c21
aditya.fauzian <aditya.fauzian@pactindo.com>
2024-07-30 14:50:48 +0700

    Remove button on page PO as Finance role

--- app/purchase-order/TopSectionRightSide.tsx
+++ app/purchase-order/TopSectionRightSide.tsx
@@ -2,8 +2,18 @@
 
 import { Button } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 
 const TopSectionRightSide = () => {
+
+  const [username, setUsername] = useState<string | null>(null);
+
+  useEffect(() => {
+    const storedUsername = localStorage.getItem("username");
+    if (storedUsername) {
+      setUsername(storedUsername);
+    }
+  }, []);
   const router = useRouter();
   return (
     <div className="flex justify-normal gap-2">
@@ -15,12 +25,14 @@ const TopSectionRightSide = () => {
         Segarkan
       </Button>
 
-      <Button
-        onClick={() => router.push("/purchase-order/form")}
-        className="bg-[#00a110] font-semibold text-white"
-      >
-        Tambah
-      </Button>
+      {username === "admin" && (
+        <Button
+          onClick={() => router.push("/proforma-invoice-dua/form")}
+          className="bg-[#00a110] font-semibold text-white"
+        >
+          Tambah
+        </Button>
+      )}
     </div>
   );
 
