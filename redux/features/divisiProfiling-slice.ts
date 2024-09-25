b58c1b3784fafc24b74140e60e307286ba3a6a21
aditya.fauzian <aditya.fauzian@pactindo.com>
2024-09-25 14:10:57 +0700

    Fixing : Build Error on File redux/features/divisiProfiling-slice.ts

--- redux/features/divisiProfiling-slice.ts
+++ redux/features/divisiProfiling-slice.ts
@@ -1,30 +1,25 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-type InitialState = {
-  divisi: any;
-  value: DivisiProfilingState;
-};
-
 type DivisiProfilingState = {
   divisi: string;
 };
 
-const initialState = {
+type InitialState = {
+  value: DivisiProfilingState;
+};
+
+const initialState: InitialState = {
   value: {
     divisi: "",
-  } as DivisiProfilingState,
-} as InitialState;
+  },
+};
 
 export const divisiProfiling = createSlice({
   name: "divisiProfiling",
   initialState,
   reducers: {
-    setDivisiProfiling: (_state, action: PayloadAction<string>) => {
-      return {
-        value: {
-          divisi: action.payload,
-        },
-      };
+    setDivisiProfiling: (state, action: PayloadAction<string>) => {
+      state.value.divisi = action.payload;
     },
   },
 });
